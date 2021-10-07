167ee4aaa05a7ab9a790ebdf2dbd03bf9cee6c1e
Aidan Tilgner <aidantilgner02@gmail.com>
2021-10-07 14:38:54 -0700

    initial commit
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //  Defines cout
+
+using std::cout; // Another way of declaring the namespace and functions in the namespace
+
+int main(){ //  Main function, type = integer
+    std::cout << "Hello World 1\n"; 
+    /**  
+     *  std = namespace, function = cout, avoids naming conflicts 
+     *  best practice is using the std:: prefix because it avoids naming conflicts
+     *  cout is an object, uses the ostream class
+     *  << is an operator, works with operands cout and "Hello World"
+    */
+
+    using namespace std; // Using directive...bad practice, can introduce naming conflicts
+    cout << "Hello World 2\n";
+    /**
+     * Another way to define a namespace is for a block, like so
+    */ 
+
+    return 0;
+}
---
+++ variables/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int slices = 5; 
+    /**
+     * int is the datatype
+     * slices is the variable name
+     * = is the assigment operator
+     * 5 is the value
+    */
+    cout << "There are " << slices << " slices of pizza" << endl; // endl is a manipulator that goes to the next line, like "\n"
+
+    return 0;
+}
