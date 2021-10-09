f8948b5af79bd273e1666b9bf64bd1f4a5b44f0a
Aidan Tilgner <aidantilgner02@gmail.com>
2021-10-09 13:56:10 -0700

    changes
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ 9-Literals/quoted.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    //  Quoted literals
+    auto x = 5U;
+    //  Unsigned interger datatype is assumed, still can't change it to other things
+
+    auto y = 5ULL;
+    //  Unsigned long long 
+
+    std::cout << x;
+
+    return 0;
+}
