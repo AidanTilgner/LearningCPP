5218873a590c708857e3a4c788e870e139aadac6
Aidan Tilgner <aidantilgner02@gmail.com>
2021-10-10 16:28:12 -0700

    added info
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- 13-Arrays/array.cpp
+++ 13-Arrays/array.cpp
@@ -5,7 +5,8 @@ int main() {
   int guesses[20]; // The 20 declares the amount of memory allocated, as in 20
                    // integers
 
-  int size = sizeof guesses;  // The size of the array
+  int size = sizeof guesses; // The size of the array
+                             // Sizeof is lost when passed to a function
 
   return 0;
 }
